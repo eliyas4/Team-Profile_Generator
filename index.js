5d8bc2a007ca8d07d33ac8240cea1d1669ee4793
Eliyas <eliyas.kapasi@gmail.com>
2023-03-01 21:42:58 +0000

    installed inquirer

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+//const Engineer = require("./lib/Engineer");
+//const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,4 +13,16 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+let team = [];
+
+startProgram()
+async function startProgram() {
+
+    team.push(new Employee("hiuj", 1, "yhgi"))
+
+    let htmlDoc = render(team)
+
+    await fs.writeFile(outputPath, htmlDoc)
+}
 

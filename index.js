aec5eda14ff664a8802f3ed18cbc88ec6d31351e
Eliyas <eliyas.kapasi@gmail.com>
2023-03-03 21:50:30 +0000

    finished the inquirer code and added the start program function so the application runs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Manager = require("./lib/Manager");
-//const Engineer = require("./lib/Engineer");
-//const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
@@ -10,10 +10,35 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 
+let team = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 managerInquiry()
+
+function pickEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to add another type of employee?",
+            name: "employeeType",
+            choices: ["Intern", "Engineer", "The team is complete!"],
+        }
+    ])
+    .then((response) => {
+        if (response.employeeType === "Intern") {
+            internInquiry();
+        }
+        else if (response.employeeType === "Engineer") {
+            engineerInquiry();            
+        }
+        else {
+            console.log("Your team is being built!")
+            startProgram();
+        }
+    })
+};
 
 function managerInquiry() {
     inquirer.prompt([
@@ -45,10 +70,80 @@ function managerInquiry() {
             response.email,
             response.officeNumber,
         );
+        team.push(manager)
+        pickEmployee()
     });
-    
 };
 
+function engineerInquiry() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please enter the engineer's name",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is their employee ID number?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "Please enter the employee's Email address",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Please enter the employee's GitHub username",
+            name: 'officeNumber',
+        },
+    ])
+    .then((response) => {
+        const engineer = new Engineer(
+            response.name,
+            response.id,
+            response.email,
+            response.officeNumber,
+        );
+        team.push(engineer)
+        pickEmployee()
+    });
+}
+
+function internInquiry() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "Please enter the intern's name",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is their employee ID number?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "Please enter the employee's Email address",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "Please enter the employee's GitHub username",
+            name: 'officeNumber',
+        },
+    ])
+    .then((response) => {
+        const intern = new Intern(
+            response.name,
+            response.id,
+            response.email,
+            response.officeNumber,
+        );
+        team.push(intern)
+        pickEmployee()
+    });
+}
 
 
 
@@ -64,15 +159,12 @@ function managerInquiry() {
 
 
 
-let team = [];
 
-startProgram()
+
 async function startProgram() {
 
-    team.push(new Employee("hiuj", 1, "yhgi"))
-
     let htmlDoc = render(team)
-
     await fs.writeFile(outputPath, htmlDoc)
+
 }
 
